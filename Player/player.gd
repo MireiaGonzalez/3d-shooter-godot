4df9a03d1076fe2efecdb2271601232504d0bfbb
Mireia <mireiagonzalezz03@gmail.com>
2025-09-02 20:33:26 +0200

    added damage animation for when player gets hit

--- Player/player.gd
+++ Player/player.gd
@@ -5,12 +5,16 @@ extends CharacterBody3D
 @export var max_hitpoints := 100
 
 @onready var camera_pivot: Node3D = $CameraPivot
+@onready var damage_animation_player: AnimationPlayer = $DamageTexture/DamageAnimationPlayer
 
 const SPEED = 5.0
 
 var mouse_motion := Vector2.ZERO
 var hitpoints: int = max_hitpoints:
 	set(value):
+		if value < hitpoints:
+			damage_animation_player.stop(false)
+			damage_animation_player.play("TakeDamage")
 		hitpoints = value
 		print(hitpoints)
 		if hitpoints <= 0:
