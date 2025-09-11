91f15c61851ad5fe18d6a30ab5a830ffab330467
Mireia <mireiagonzalezz03@gmail.com>
2025-09-11 20:57:29 +0200

    added ammo tracking system

---
+++ Player/ammo_handler.gd
@@ -0,0 +1,21 @@
+class_name AmmoHandler extends Node
+
+enum ammo_type {
+	BULLET,
+	SMALL_BULLET,
+	PISTOL_BULLETS
+}
+
+var ammo_storage := {
+	ammo_type.BULLET: 10,
+	ammo_type.SMALL_BULLET: 60,
+	ammo_type.PISTOL_BULLETS: 100
+}
+
+func has_ammo(bullet_type: ammo_type) -> bool:
+	return ammo_storage[bullet_type] > 0
+
+func use_ammo(bullet_type: ammo_type) -> void:
+	if has_ammo(bullet_type):
+		ammo_storage[bullet_type] -= 1
+	print(ammo_storage[bullet_type])
